d4db6cf928f668312e45fb9c3e0d6c9045fda3e5
ponkichi0111 <z.hiroyuki.0111@gmail.com>
2025-07-21 01:43:51 +0900

    update: backup

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# todo-app-terraform
+# ToDoアプリを作成
+
+## 事前準備
+・アクセスキーを使用せずにterraformを実行するため、AWS IAM Identity Centerを利用する。
+1. **Identity Centerでユーザー作成**
+- Terraformを実行するための専用ユーザーをAWS Identity Centerで作成
+
+2. **ローカル設定**
+- [~/.aws/config]にアカウントID、ロール名を追加
+  ```
+  [profile terraform-admin]
+  sso_start_url = https://my-sso-portal.awsapps.com/start
+  sso_region = ap-northeast-1
+  sso_account_id = 123456789012
+  sso_role_name = TerraformDeployerRole
+  region = ap-northeast-1
+  output = json
+  ```
--- infra/terraform/backend/main.tf
+++ infra/terraform/backend/main.tf
@@ -1,17 +1,10 @@
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "terraform-admin"
+}
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   lifecycle {
     prevent_destroy = true
@@ -20,6 +13,23 @@ resource "aws_s3_bucket" "terraform_state" {
   tags = {
     Name        = var.bucket_name
     Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
