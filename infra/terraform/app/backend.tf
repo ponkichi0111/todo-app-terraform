d1f189d96d2cd15d968d4bdb281e288a409d9aba
ponkichi0111 <z.hiroyuki.0111@gmail.com>
2025-07-22 01:15:30 +0900

    update:backend.tf

--- infra/terraform/app/backend.tf
+++ infra/terraform/app/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-todo-app"
+    bucket         = "terraform-state-todo-app-hiroyuki"
     key            = "app/terraform.tfstate"
     region         = "ap-northeast-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
